f14c35dbdcc0b1a14cd7b6daa90b3c645eb9b005
Youssif Hany <youssifhany962@gmail.com>
2024-09-26 21:00:51 +0300

    Initail Commit

---
+++ banquemisr.challenge05Tests/banquemisr_challenge05Tests.swift
@@ -0,0 +1,17 @@
+//
+//  banquemisr_challenge05Tests.swift
+//  banquemisr.challenge05Tests
+//
+//  Created by Youssif Hany on 26/09/2024.
+//
+
+import Testing
+@testable import banquemisr_challenge05
+
+struct banquemisr_challenge05Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
