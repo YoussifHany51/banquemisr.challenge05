2990fd260c3956481acfb197e5f5072b873670f3
Youssif Hany <youssifhany962@gmail.com>
2024-09-27 16:42:11 +0300

    PopularView / UpcomingView Created

--- banquemisr.challenge05/Modules/Upcoming/View/UpcomingViewController.swift
+++ banquemisr.challenge05/Modules/Upcoming/View/UpcomingViewController.swift
@@ -9,21 +9,71 @@ import UIKit
 
 class UpcomingViewController: UIViewController {
 
+    @IBOutlet weak var upcomingTableView: UITableView!
+    let viewModel = MoviesViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUpData()
+    }
+    private func setUpData(){
+        upcomingTableView.dataSource = self
+        upcomingTableView.delegate = self
+        navigationItem.title = "Popular"
+        setupCell()
+        loadData()
+    }
+    private func loadData(){
+        viewModel.fetchData(movieType: .upcoming){ [weak self] in
+            DispatchQueue.main.async {
+                self?.upcomingTableView.reloadData()
+            }
+        }
+    }
+    private func setupCell(){
+        let nowPlayingCellNib = UINib(nibName: "MoviesTableViewCell", bundle: nil)
+        upcomingTableView.register(nowPlayingCellNib, forCellReuseIdentifier: "moviesCell")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func loadImage(from url: URL, completion: @escaping (UIImage?) -> Void) {
+        DispatchQueue.global().async {
+            if let data = try? Data(contentsOf: url), let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    completion(image)
+                }
+            } else {
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+            }
+        }
     }
-    */
-
+}
+extension UpcomingViewController:UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "moviesCell", for: indexPath) as! MoviesTableViewCell
+        cell.titleLabel.text = viewModel.movies[indexPath.row].title
+        cell.releaseDateLabel.text = "Release Date : \(viewModel.movies[indexPath.row].release_date)"
+        
+        let posterPath = viewModel.movies[indexPath.row].poster_path
+        let imageUrl = "https://image.tmdb.org/t/p/w500\(posterPath)"
+        loadImage(from: URL(string: imageUrl)!) { image in
+            if let image = image {
+                cell.imageLabel?.image = image
+            }
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
 }
